11fbbba6f64ab1940b755e5bc01433a52040da24
markumina <mark.umina@gmail.com>
2020-10-13 02:00:57 -0400

    Add calculations and more parsing.

--- main/nvs_blob_example_main.c
+++ main/nvs_blob_example_main.c
@@ -1,226 +1,214 @@
-/* Non-Volatile Storage (NVS) Read and Write a Blob - Example
-
-   For other examples please check:
-   https://github.com/espressif/esp-idf/tree/master/examples
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "nvs.h"
-#include "driver/gpio.h"
 
-// GPS Example
 #include "gps_data.h"
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <math.h>
 
-#define STORAGE_NAMESPACE "storage"
+void parse_gps_data(void);
 
-/* Save the number of module restarts in NVS
-   by first reading and then incrementing
-   the number that has been saved previously.
-   Return an error if anything goes wrong
-   during this process.
- */
-esp_err_t save_restart_counter(void)
-{
-    nvs_handle_t my_handle;
-    esp_err_t err;
+// From lib
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::  Function prototypes                                           :*/
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+#define pi 3.14159265358979323846
+double deg2rad(double);
+double rad2deg(double);
+double distance(double lat1, double lon1, double lat2, double lon2, char unit);
+// End From lib
 
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle);
-    if (err != ESP_OK) return err;
-
-    // Read
-    int32_t restart_counter = 0; // value will default to 0, if not set yet in NVS
-    err = nvs_get_i32(my_handle, "restart_conter", &restart_counter);
-    if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-    // Write
-    restart_counter++;
-    err = nvs_set_i32(my_handle, "restart_conter", restart_counter);
-    if (err != ESP_OK) return err;
-
-    // Commit written value.
-    // After setting any values, nvs_commit() must be called to ensure changes are written
-    // to flash storage. Implementations may write to storage at other times,
-    // but this is not guaranteed.
-    err = nvs_commit(my_handle);
-    if (err != ESP_OK) return err;
-
-    // Close
-    nvs_close(my_handle);
-    return ESP_OK;
-}
-
-/* Save new run time value in NVS
-   by first reading a table of previously saved values
-   and then adding the new value at the end of the table.
-   Return an error if anything goes wrong
-   during this process.
- */
-esp_err_t save_run_time(void)
-{
-    nvs_handle_t my_handle;
-    esp_err_t err;
-
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle);
-    if (err != ESP_OK) return err;
-
-    // Read the size of memory space required for blob
-    size_t required_size = 0;  // value will default to 0, if not set yet in NVS
-    err = nvs_get_blob(my_handle, "run_time", NULL, &required_size);
-    if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-
-    // Read previously saved blob if available
-    uint32_t* run_time = malloc(required_size + sizeof(uint32_t));
-    if (required_size > 0) {
-        err = nvs_get_blob(my_handle, "run_time", run_time, &required_size);
-        if (err != ESP_OK) {
-            free(run_time);
-            return err;
-        }
-    }
-
-    // Write value including previously saved blob if available
-    required_size += sizeof(uint32_t);
-    run_time[required_size / sizeof(uint32_t) - 1] = xTaskGetTickCount() * portTICK_PERIOD_MS;
-    err = nvs_set_blob(my_handle, "run_time", run_time, required_size);
-    free(run_time);
-
-    if (err != ESP_OK) return err;
-
-    // Commit
-    err = nvs_commit(my_handle);
-    if (err != ESP_OK) return err;
-
-    // Close
-    nvs_close(my_handle);
-    return ESP_OK;
-}
-
-/* Read from NVS and print restart counter
-   and the table with run times.
-   Return an error if anything goes wrong
-   during this process.
- */
-esp_err_t print_what_saved(void)
-{
-    nvs_handle_t my_handle;
-    esp_err_t err;
-
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle);
-    if (err != ESP_OK) return err;
-
-    // Read restart counter
-    int32_t restart_counter = 0; // value will default to 0, if not set yet in NVS
-    err = nvs_get_i32(my_handle, "restart_conter", &restart_counter);
-    if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-    printf("Restart counter = %d\n", restart_counter);
-
-    // Read run time blob
-    size_t required_size = 0;  // value will default to 0, if not set yet in NVS
-    // obtain required memory space to store blob being read from NVS
-    err = nvs_get_blob(my_handle, "run_time", NULL, &required_size);
-    if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) return err;
-    printf("Run time:\n");
-    if (required_size == 0) {
-        printf("Nothing saved yet!\n");
-    } else {
-        uint32_t* run_time = malloc(required_size);
-        err = nvs_get_blob(my_handle, "run_time", run_time, &required_size);
-        if (err != ESP_OK) {
-            free(run_time);
-            return err;
-        }
-        for (int i = 0; i < required_size / sizeof(uint32_t); i++) {
-            printf("%d: %d\n", i + 1, run_time[i]);
-        }
-        free(run_time);
-    }
-
-    // Close
-    nvs_close(my_handle);
-    return ESP_OK;
-}
-
+#define MAX_CONTINUOUS_WALK_BREAK 3000U
 
 void app_main()
 {
-    esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        // NVS partition was truncated and needs to be erased
-        // Retry nvs_flash_init
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        err = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK( err );
+    parse_gps_data();
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+    esp_restart();
+}
 
-    err = print_what_saved();
-    if (err != ESP_OK) printf("Error (%s) reading data from NVS!\n", esp_err_to_name(err));
+void parse_gps_data(void)
+{
+    printf("\nLength of GPS data: %d\n\n", sizeof(gps_data));
 
-    err = save_restart_counter();
-    if (err != ESP_OK) printf("Error (%s) saving restart counter to NVS!\n", esp_err_to_name(err));
+    // Get line
+    char* gps_data_copy = (char*)malloc(sizeof(gps_data));
+    strcpy(gps_data_copy, gps_data);
+    char* end_data_line = NULL;
+    char* end_date = NULL;
+    char* gps_data_line = strtok_r(gps_data_copy, "\n", &end_data_line);
+    char gps_data_line_copy[1000] = {0};
+    char* gps_date = NULL;
 
-    gpio_pad_select_gpio(GPIO_NUM_0);
-    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_DEF_INPUT);
+    // Lat/lon strings
+    float current_latitude = 0.0;
+    float current_longitude = 0.0;
+    float previous_latitude = 0.0;
+    float previous_longitude = 0.0;
 
-    /* Read the status of GPIO0. If GPIO0 is LOW for longer than 1000 ms,
-       then save module's run time and restart it
-     */
-    while (1) {
-        if (gpio_get_level(GPIO_NUM_0) == 0) {
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            if(gpio_get_level(GPIO_NUM_0) == 0) {
-                err = save_run_time();
-                if (err != ESP_OK) printf("Error (%s) saving run time blob to NVS!\n", esp_err_to_name(err));
-                printf("Restarting...\n");
+    // Accumulators
+    float accumuated_trip_distance = 0.0;
+    uint32_t number_of_walks = 0;
 
-#if 1
-                // Work with GPS data
-                printf("\nLength of GPS data: %d\n\n", sizeof(gps_data));
+    // For timestamp
+    struct tm tm = {0};
+    time_t current_epoch = 0;
+    time_t last_epoch = 0;
 
-                // Get line
-                char* gps_data_copy = (char*)malloc(sizeof(gps_data));
-                strcpy(gps_data_copy, gps_data);
-                char* end_data_line = NULL;
-                char* end_date = NULL;
-                char* gps_data_line = strtok_r(gps_data_copy, "\n", &end_data_line);
-                char gps_data_line_copy[1000] = {0};
-                char* gps_date = NULL;
+    // Flags
+    bool b_first_pass = true;
 
-                // Process line
-                while(gps_data_line != NULL)
+    // Process line
+    while(gps_data_line != NULL)
+    {
+#ifdef DEBUG_PARSE
+        printf("%s\n", gps_data_line);
+#endif // DEBUG_PARSE
+        memcpy(gps_data_line_copy, gps_data_line, strlen(gps_data_line));
+
+        // + or ; here? ; doesn't hurt..
+        gps_date = strtok_r(gps_data_line_copy, ";", &end_date);
+
+        if(gps_date != NULL)
+        {
+#ifdef DEBUG_PARSE
+            printf("\n%s\n", gps_date);
+#endif // DEBUG_PARSE
+
+            if(strptime(gps_date, "%Y-%m-%d %H:%M:%S ", &tm) != NULL)
+            {
+                current_epoch = mktime(&tm);
+
+                // Initialize last_epoch with first for first pass..
+                // -- b_firstpass gets set at end of first pass through loop!
+                if(true == b_first_pass)
                 {
-                    //printf("%s\n", gps_data_line);
-#if 1
-                    memcpy(gps_data_line_copy, gps_data_line, strlen(gps_data_line));
-
-                    gps_date = strtok_r(gps_data_line_copy, "+", &end_date);
-                    if(gps_date != NULL)
-                    {
-                        printf("\n%s\n", gps_date);
-                    }
-#endif // 0
-
-                    gps_data_line = strtok_r(NULL, "\n", &end_data_line);
+                    last_epoch = current_epoch;
                 }
-#endif // 0
 
-                fflush(stdout);
-                esp_restart();
+#ifdef DEBUG_PARSE
+                printf("Epoch time current: %jd\nEpoch time last: %jd\nEpoch time diff: %jd\n"
+                     ,(intmax_t)current_epoch
+                     ,(intmax_t)last_epoch
+                     ,(intmax_t)current_epoch - (intmax_t)last_epoch);
+#endif // DEBUG_PARSE
+
+                if(current_epoch - last_epoch > MAX_CONTINUOUS_WALK_BREAK)
+                {
+                    // TBD - check distance between this walk and the last, to diff between rest and walk.
+#ifdef DEBUG_PARSE
+                    printf("\nFound walk number: %d??\n\n", ++number_of_walks);
+#endif // DEBUG_PARSE
+                }
+
+                last_epoch = current_epoch;
             }
         }
-        vTaskDelay(200 / portTICK_PERIOD_MS);
+
+        // Extract coordinates from line..
+        current_latitude = atof(strtok_r(NULL, ";", &end_date));
+        current_longitude = atof(strtok_r(NULL, ";", &end_date));
+
+#ifdef DEBUG_PARSE
+        printf("\n\nlatitude: %lfF\nlongititude: %lfF\n" ,current_latitude ,current_longitude);
+#endif // DEBUG_PARSE
+
+        if(true == b_first_pass)
+        {
+            previous_latitude = current_latitude;
+            previous_longitude = current_longitude;
+        }
+
+        accumuated_trip_distance += distance(previous_latitude, previous_longitude, current_latitude, current_longitude, 'M');
+
+        previous_latitude = current_latitude;
+        previous_longitude = current_longitude;
+
+        b_first_pass = false;
+
+        gps_data_line = strtok_r(NULL, "\n", &end_data_line);
+
+    } // while
+
+    printf("\n\n******* Results *******\n\n");
+    printf("\nTotal number of walks . . . . . . . . .  %d\n", number_of_walks);
+    printf("\nTotal miles walked . . . . . . . . . . . %lf\n", accumuated_trip_distance);
+    // TBD: Make these cached variables..
+    printf("\nStart/End straight line distance . . . . %lf\n", distance(40.702452, -73.984135, 40.736956, -73.986960, 'M'));
+    printf("\nTime spent traveling. . . . . . . . . .  TBD\n");
+    printf("\nTime spent at rest . . . . . . . . . . . TBD\n");
+
+    printf("\n\nNOTE:\nTotal number of walks based on %d second break.", MAX_CONTINUOUS_WALK_BREAK);
+    printf("\n");
+}
+
+// TBD, separate into file..
+
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::                                                                         :*/
+/*::  This routine calculates the distance between two points (given the     :*/
+/*::  latitude/longitude of those points). It is being used to calculate     :*/
+/*::  the distance between two locations using GeoDataSource(TM) products.   :*/
+/*::                                                                         :*/
+/*::  Definitions:                                                           :*/
+/*::    South latitudes are negative, east longitudes are positive           :*/
+/*::                                                                         :*/
+/*::  Passed to function:                                                    :*/
+/*::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :*/
+/*::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :*/
+/*::    unit = the unit you desire for results                               :*/
+/*::           where: 'M' is statute miles (default)                         :*/
+/*::                  'K' is kilometers                                      :*/
+/*::                  'N' is nautical miles                                  :*/
+/*::  Worldwide cities and other features databases with latitude longitude  :*/
+/*::  are available at https://www.geodatasource.com                         :*/
+/*::                                                                         :*/
+/*::  For enquiries, please contact sales@geodatasource.com                  :*/
+/*::                                                                         :*/
+/*::  Official Web site: https://www.geodatasource.com                       :*/
+/*::                                                                         :*/
+/*::           GeoDataSource.com (C) All Rights Reserved 2018                :*/
+/*::                                                                         :*/
+/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+
+double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
+  double theta, dist;
+  if ((lat1 == lat2) && (lon1 == lon2)) {
+    return 0;
+  }
+  else {
+    theta = lon1 - lon2;
+    dist = sin(deg2rad(lat1)) * sin(deg2rad(lat2)) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * cos(deg2rad(theta));
+    dist = acos(dist);
+    dist = rad2deg(dist);
+    dist = dist * 60 * 1.1515;
+    switch(unit) {
+      case 'M':
+        break;
+      case 'K':
+        dist = dist * 1.609344;
+        break;
+      case 'N':
+        dist = dist * 0.8684;
+        break;
     }
+    return (dist);
+  }
+}
+
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::  This function converts decimal degrees to radians             :*/
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+double deg2rad(double deg) {
+  return (deg * pi / 180);
+}
+
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+/*::  This function converts radians to decimal degrees             :*/
+/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+double rad2deg(double rad) {
+  return (rad * 180 / pi);
 }
