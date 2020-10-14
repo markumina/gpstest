91bd5bb77c1fb51a9b3243ab8fec246a4261b366
markumina <mark.umina@gmail.com>
2020-10-14 02:12:46 -0400

    Separate library from main code.

---
+++ main/geodatasource.h
@@ -0,0 +1,8 @@
+#ifndef _GEODATASOURCE_H
+#define _GEODATASOURCE_H
+
+#include <stdio.h>
+
+double distance(double lat1, double lon1, double lat2, double lon2, char unit);
+
+#endif // _GEODATASOURCE_H
